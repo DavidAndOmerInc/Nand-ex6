674df1a5bd3e4ca28ed60fd13be8c45b45ab4188
Omer <Omertorrents@gmail.com>
2016-11-27 18:14:02 +0200

    omer is cool

--- pythonParser.py
+++ pythonParser.py
@@ -1,1 +1,13 @@
-test
+class hackFile:
+    def __init__(self, fileToParse):
+        __variables, __lines = parseLines(fileToParse)
+
+    def parseLines(self, lines):
+        lines = lines.split("\n")
+
+
+
+
+
+
+
